2419ff83360f8cc3be6b5872fdc44a4bfbe09110
nloeser_msc2023 <nloeser_msc2023@luke.ifi.uni-heidelberg.de>
2024-04-21 14:55:37 +0000

    fix pytorch and sklearn examples

---
+++ update-results-metadata.py
@@ -0,0 +1,27 @@
+from argparse import ArgumentParser
+import json
+from os import listdir, path
+
+def main(args):
+    with open(args.metadata, "r") as f:
+        items = json.loads(f.read())
+        metadata = {item["task_name"]: item for item in items}
+    for p in listdir(args.results):
+        p = path.join(args.results, p)
+        with open(p, "r") as f:
+            item = json.loads(f.read())
+        for i in item["items"]:
+            mi = metadata[i["task_name"]]
+            for k, v in mi.items():
+                i[k] = v
+        with open(p, "w") as f:
+            f.write(json.dumps(item))
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("-r", "--results", default="dataset/results")
+    parser.add_argument("-m", "--metadata")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main(parse_args())
